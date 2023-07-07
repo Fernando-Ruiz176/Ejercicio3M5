8f7fc51d53aed5aaf43de71464386d83de959a5a
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2023-07-07 04:15:41 -0400

    Diseño completado.

--- app/src/main/java/cl/fernando/ejercicio3m5/MainActivity.java
+++ app/src/main/java/cl/fernando/ejercicio3m5/MainActivity.java
@@ -2,13 +2,20 @@ package cl.fernando.ejercicio3m5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
